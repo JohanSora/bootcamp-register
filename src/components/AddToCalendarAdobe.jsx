d71514cdb5bc4c707dbe50a724523a6aabd8ecbd
JohanSora <johan.sora@mymarketlogic.com>
2022-09-20 08:54:18 -0500

    fixed adobe canal

---
+++ src/components/AddToCalendarAdobe.jsx
@@ -0,0 +1,205 @@
+import React from "react";
+
+const AddToCalendarAdobe = () => {
+  return (
+    <div>
+      <p className="title-add-to-calendar">
+        Puedes añadir este evento a tu calendario favorito
+      </p>
+      <p className="title-add-to-calendar">México:</p>
+
+      <p className="container-imgs-addtocalendar">
+        <a
+          href={"https://www.addevent.com/event/em14903704+apple"}
+          title="Apple"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-apple-t1.png"
+            alt="Apple"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/em14903704+google"}
+          title="Google"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-google-t1.png"
+            alt="Google"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/em14903704+office365"}
+          title="Office 365"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-office365-t1.png"
+            alt="Office 365"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/em14903704+outlook"}
+          title="Outlook"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-outlook-t1.png"
+            alt="Outlook"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/em14903704+outlookcom"}
+          title="Outlook.com"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-outlookcom-t1.png"
+            alt="Outlook.com"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/em14903704+yahoo"}
+          title="Yahoo"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-yahoo-t1.png"
+            alt="Yahoo"
+          />
+        </a>
+      </p>
+
+      <p className="title-add-to-calendar">Chile:</p>
+
+      <p className="container-imgs-addtocalendar">
+        <a
+          href={"https://www.addevent.com/event/Xa14903729+apple"}
+          title="Apple"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-apple-t1.png"
+            alt="Apple"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/Xa14903729+google"}
+          title="Google"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-google-t1.png"
+            alt="Google"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/Xa14903729+office365"}
+          title="Office 365"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-office365-t1.png"
+            alt="Office 365"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/Xa14903729+outlook"}
+          title="Outlook"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-outlook-t1.png"
+            alt="Outlook"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/Xa14903729+outlookcom"}
+          title="Outlook.com"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-outlookcom-t1.png"
+            alt="Outlook.com"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/Xa14903729+yahoo"}
+          title="Yahoo"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-yahoo-t1.png"
+            alt="Yahoo"
+          />
+        </a>
+      </p>
+      <p className="title-add-to-calendar">Colombia:</p>
+      <p className="container-imgs-addtocalendar">
+        <a
+          href={"https://www.addevent.com/event/WU14903544+apple"}
+          title="Apple"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-apple-t1.png"
+            alt="Apple"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/WU14903544+google"}
+          title="Google"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-google-t1.png"
+            alt="Google"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/WU14903544+office365"}
+          title="Office 365"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-office365-t1.png"
+            alt="Office 365"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/WU14903544+outlook"}
+          title="Outlook"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-outlook-t1.png"
+            alt="Outlook"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/WU14903544+outlookcom"}
+          title="Outlook.com"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-outlookcom-t1.png"
+            alt="Outlook.com"
+          />
+        </a>
+        <a
+          href={"https://www.addevent.com/event/WU14903544+yahoo"}
+          title="Yahoo"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.addevent.com/libs/imgs/icon-emd-share-yahoo-t1.png"
+            alt="Yahoo"
+          />
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default AddToCalendarAdobe;
